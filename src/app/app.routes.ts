9fc7c8fd373ea1857efe7dcdf21bc5a172f795e0
Jonathan Sousa <jonathan_msi@live.com>
2025-11-06 11:39:06 +0000

    Basic angular project structure creation

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { IconicTemplateComponent } from './template/iconic/iconic-template/iconic-template.component';
+import { IconicAboutComponent } from './template/iconic/iconic-about/iconic-about.component';
+import { IconicServiceComponent } from './template/iconic/iconic-service/iconic-service.component';
+import { IconicBlogComponent } from './template/iconic/iconic-blog/iconic-blog.component';
+import { IconicContactComponent } from './template/iconic/iconic-contact/iconic-contact.component';
+import { HomeIconicComponent } from './template/iconic/iconic-home/home-iconic.component';
+
+export const routes: Routes = [
+    {
+        path: 'iconic/home', component: HomeIconicComponent
+    },
+    {
+        path: 'iconic',
+        component: IconicTemplateComponent,
+        children: [
+            { path: 'about', component: IconicAboutComponent },
+            { path: 'services', component: IconicServiceComponent },
+            { path: 'blog', component: IconicBlogComponent },
+            { path: 'contact', component: IconicContactComponent }
+        ]
+    }
+];
