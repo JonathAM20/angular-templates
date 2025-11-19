8d0647c6ff4b73074060e47507355df770fea8de
Jonathan Sousa <jonathan_msi@live.com>
2025-11-19 12:26:19 +0000

    Blog page update

--- src/app/template/iconic/iconic-blog/iconic-blog.component.ts
+++ src/app/template/iconic/iconic-blog/iconic-blog.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { CardListWithPaginationComponent } from "./internal-components/card-list-with-pagination/card-list-with-pagination.component";
 
 @Component({
   selector: 'app-iconic-blog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    CardListWithPaginationComponent
+],
   templateUrl: './iconic-blog.component.html',
   styleUrl: './iconic-blog.component.css'
 })
-export class IconicBlogComponent {
+export class IconicBlogComponent implements OnInit, OnDestroy {
+
+  isVisible: boolean = false;
+
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef) { }
+
+  handleIntersection(entries: IntersectionObserverEntry[]): void {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.isVisible = true;
+        this.observer.unobserve(this.el.nativeElement);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    const options = {
+      root: null,
+      rootMargin: '1000px',
+      threshold: 0.2
+    };
+
+    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) {
+      this.observer.unobserve(this.el.nativeElement);
+    }
+  }
 
 }
