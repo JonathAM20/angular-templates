d9cb58ff32ae8fad46a519e22036d73618907079
Jonathan Sousa <jonathan_msi@live.com>
2025-11-19 09:54:22 +0000

    Fade in up set to header class on About page

--- src/app/template/iconic/iconic-about/iconic-about.component.ts
+++ src/app/template/iconic/iconic-about/iconic-about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { AboutPitch2Component } from "./internal-components/about-pitch2/about-pitch2.component";
 import { AboutPitch3Component } from "./internal-components/about-pitch3/about-pitch3.component";
 import { Team1Component } from "./internal-components/team1/team1.component";
@@ -16,4 +16,38 @@ import { AboutPitch1Component } from "./internal-components/about-pitch1/about-p
   templateUrl: './iconic-about.component.html',
   styleUrl: './iconic-about.component.css'
 })
-export class IconicAboutComponent {}
+export class IconicAboutComponent implements OnInit, OnDestroy {
+
+  isVisible: boolean = false;
+
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef) { }
+
+  handleIntersection(entries: IntersectionObserverEntry[]): void {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.isVisible = true;
+        this.observer.unobserve(this.el.nativeElement);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    const options = {
+      root: null,
+      rootMargin: '1000px',
+      threshold: 0.2
+    };
+
+    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) {
+      this.observer.unobserve(this.el.nativeElement);
+    }
+  }
+
+}
