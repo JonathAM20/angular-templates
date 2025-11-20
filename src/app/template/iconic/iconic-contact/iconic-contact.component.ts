34b417b3cc58b22f368b0b45723d82d0d05c4a89
Jonathan Sousa <jonathan_msi@live.com>
2025-11-20 09:31:44 +0000

    Contact page update

--- src/app/template/iconic/iconic-contact/iconic-contact.component.ts
+++ src/app/template/iconic/iconic-contact/iconic-contact.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { ContactComponent } from "../../../contact/contact.component";
 
 @Component({
   selector: 'app-iconic-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './iconic-contact.component.html',
   styleUrl: './iconic-contact.component.css'
 })
-export class IconicContactComponent {
+export class IconicContactComponent implements OnInit, OnDestroy {
+
+  isVisible: boolean = false;
+
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef) { }
+
+  handleIntersection(entries: IntersectionObserverEntry[]): void {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.isVisible = true;
+        this.observer.unobserve(this.el.nativeElement);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    const options = {
+      root: null,
+      rootMargin: '1000px',
+      threshold: 0.2
+    };
+
+    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) {
+      this.observer.unobserve(this.el.nativeElement);
+    }
+  }
 
 }
