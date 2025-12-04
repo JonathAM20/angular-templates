ba95706d884ee33275848366d7e03f311fdf474c
Jonathan Sousa <jonathan_msi@live.com>
2025-12-04 15:21:07 +0000

    Contact Page update

--- src/app/template/iconic/iconic-contact/iconic-contact.component.ts
+++ src/app/template/iconic/iconic-contact/iconic-contact.component.ts
@@ -1,45 +1,23 @@
-import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ContactComponent } from "./internal-components/contact/contact.component";
+import { VisibilityObserverDirective } from '../../../visibility-observer.directive';
 
 @Component({
   selector: 'app-iconic-contact',
   standalone: true,
-  imports: [ContactComponent],
+  imports: [
+    ContactComponent,
+    VisibilityObserverDirective
+  ],
   templateUrl: './iconic-contact.component.html',
   styleUrl: './iconic-contact.component.css'
 })
-export class IconicContactComponent implements OnInit, OnDestroy {
+export class IconicContactComponent {
 
   isVisible: boolean = false;
 
-  private observer!: IntersectionObserver;
-
-  constructor(private el: ElementRef) { }
-
-  handleIntersection(entries: IntersectionObserverEntry[]): void {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.isVisible = true;
-        this.observer.unobserve(this.el.nativeElement);
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    const options = {
-      root: null,
-      rootMargin: '1000px',
-      threshold: 0.2
-    };
-
-    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
-    this.observer.observe(this.el.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.unobserve(this.el.nativeElement);
-    }
+  handleVisibilityChange(isVisible: boolean): void {
+    this.isVisible = isVisible;
   }
 
 }
