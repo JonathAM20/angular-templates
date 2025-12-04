486ccded04715221307aa66b9b755c157e61772f
Jonathan Sousa <jonathan_msi@live.com>
2025-12-04 15:20:30 +0000

    About Page update

--- src/app/template/iconic/iconic-about/iconic-about.component.ts
+++ src/app/template/iconic/iconic-about/iconic-about.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AboutPitch2Component } from "./internal-components/about-pitch2/about-pitch2.component";
 import { AboutPitch3Component } from "./internal-components/about-pitch3/about-pitch3.component";
 import { Team1Component } from "./internal-components/team1/team1.component";
 import { AboutPitch1Component } from "./internal-components/about-pitch1/about-pitch1.component";
+import { VisibilityObserverDirective } from '../../../visibility-observer.directive';
 
 @Component({
   selector: 'app-iconic-about',
@@ -11,43 +12,18 @@ import { AboutPitch1Component } from "./internal-components/about-pitch1/about-p
     AboutPitch1Component,
     AboutPitch2Component,
     AboutPitch3Component,
-    Team1Component
+    Team1Component,
+    VisibilityObserverDirective
   ],
   templateUrl: './iconic-about.component.html',
   styleUrl: './iconic-about.component.css'
 })
-export class IconicAboutComponent implements OnInit, OnDestroy {
+export class IconicAboutComponent {
 
   isVisible: boolean = false;
 
-  private observer!: IntersectionObserver;
-
-  constructor(private el: ElementRef) { }
-
-  handleIntersection(entries: IntersectionObserverEntry[]): void {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.isVisible = true;
-        this.observer.unobserve(this.el.nativeElement);
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    const options = {
-      root: null,
-      rootMargin: '1000px',
-      threshold: 0.2
-    };
-
-    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
-    this.observer.observe(this.el.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.unobserve(this.el.nativeElement);
-    }
+  handleVisibilityChange(isVisible: boolean): void {
+    this.isVisible = isVisible;
   }
 
 }
