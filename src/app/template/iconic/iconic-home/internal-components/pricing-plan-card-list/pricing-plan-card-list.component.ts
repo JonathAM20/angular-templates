15cf464be07ef26919487f71b65242886514d119
Jonathan Sousa <jonathan_msi@live.com>
2025-12-10 11:02:38 +0000

    Pricing plan card list component update

--- src/app/template/iconic/iconic-home/internal-components/pricing-plan-card-list/pricing-plan-card-list.component.ts
+++ src/app/template/iconic/iconic-home/internal-components/pricing-plan-card-list/pricing-plan-card-list.component.ts
@@ -1,50 +1,26 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PricingPlan } from '../pricing-plan';
+import { VisibilityObserverDirective } from "../../../../../visibility-observer.directive";
 
 @Component({
   selector: 'app-pricing-plan-card-list',
   standalone: true,
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    VisibilityObserverDirective
+],
   templateUrl: './pricing-plan-card-list.component.html',
   styleUrl: './pricing-plan-card-list.component.css'
 })
-export class PricingPlanCardListComponent implements OnInit, OnDestroy {
+export class PricingPlanCardListComponent {
 
   @Input() pricingPlanList: PricingPlan[] = [];
 
   isVisible: boolean = false;
 
-  private observer!: IntersectionObserver;
-
-  constructor(private el: ElementRef) { }
-
-  handleIntersection(entries: IntersectionObserverEntry[]): void {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.isVisible = true;
-        this.observer.unobserve(this.el.nativeElement);
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.2
-    };
-
-    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
-    this.observer.observe(this.el.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.unobserve(this.el.nativeElement);
-    }
+  handleVisibilityChange(isVisible: boolean): void {
+    this.isVisible = isVisible;
   }
 
 }
