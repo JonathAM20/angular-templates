68eaea1763ac6aaa40ceaf2b803253c2a59f6ef8
Jonathan Sousa <jonathan_msi@live.com>
2025-12-10 10:56:11 +0000

    Service pitch card1 list component update

--- src/app/template/iconic/iconic-service/internal-components/service-pitch-card1-list/service-pitch-card1-list.component.ts
+++ src/app/template/iconic/iconic-service/internal-components/service-pitch-card1-list/service-pitch-card1-list.component.ts
@@ -1,50 +1,27 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ServicePitch } from '../service-pitch';
 import { CommonModule } from '@angular/common';
+import { VisibilityObserverDirective } from "../../../../../visibility-observer.directive";
 
 @Component({
   selector: 'app-service-pitch-card1-list',
   standalone: true,
   imports: [
     CommonModule,
+    VisibilityObserverDirective
   ],
   templateUrl: './service-pitch-card1-list.component.html',
   styleUrl: './service-pitch-card1-list.component.css'
 })
-export class ServicePitchCard1ListComponent implements OnInit, OnDestroy {
+export class ServicePitchCard1ListComponent {
 
   @Input() servicePitchList: ServicePitch[] = [];
 
   isVisible: boolean = false;
 
-  private observer!: IntersectionObserver;
-
-  constructor(private el: ElementRef) { }
-
-  handleIntersection(entries: IntersectionObserverEntry[]): void {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.isVisible = true;
-        this.observer.unobserve(this.el.nativeElement);
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.2
-    };
-
-    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
-    this.observer.observe(this.el.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.unobserve(this.el.nativeElement);
-    }
+  handleVisibilityChange(isVisible: boolean): void {
+    this.isVisible = isVisible;
+    console.log("Componente visível", this.isVisible);
   }
 
 }
