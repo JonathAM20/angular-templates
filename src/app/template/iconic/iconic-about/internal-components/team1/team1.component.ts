c28a06fdc0b1fd942221cd036d3c32fd1202abb3
Jonathan Sousa <jonathan_msi@live.com>
2025-12-10 11:07:59 +0000

    Team1 component update

--- src/app/template/iconic/iconic-about/internal-components/team1/team1.component.ts
+++ src/app/template/iconic/iconic-about/internal-components/team1/team1.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { VisibilityObserverDirective } from '../../../../../visibility-observer.directive';
 
 export interface TeamMember {
   name: string;
@@ -11,12 +12,13 @@ export interface TeamMember {
   selector: 'app-team1',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    VisibilityObserverDirective
   ],
   templateUrl: './team1.component.html',
   styleUrl: './team1.component.css'
 })
-export class Team1Component implements OnInit, OnDestroy {
+export class Team1Component {
 
   teamMemberList: TeamMember[] = [
     {
@@ -38,34 +40,8 @@ export class Team1Component implements OnInit, OnDestroy {
 
   isVisible: boolean = false;
 
-  private observer!: IntersectionObserver;
-
-  constructor(private el: ElementRef) { }
-
-  handleIntersection(entries: IntersectionObserverEntry[]): void {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        this.isVisible = true;
-        this.observer.unobserve(this.el.nativeElement);
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.2
-    };
-
-    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
-    this.observer.observe(this.el.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.unobserve(this.el.nativeElement);
-    }
+  handleVisibilityChange(isVisible: boolean): void {
+    this.isVisible = isVisible;
   }
 
 }
